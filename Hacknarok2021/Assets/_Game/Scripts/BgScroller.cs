cca04ca9e4acdb92b10c4118d8142b69314ae819
Oskar Kogut <kogutoskar123@gmail.com>
2021-03-28 00:14:42 +0100

    Add bg scrolling

---
+++ Hacknarok2021/Assets/_Game/Scripts/BgScroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgScroller : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10;
+    [SerializeField] private float _maxMove = 9;
+    
+    private void Update()
+    {
+        float amount = Input.GetAxis("Horizontal");
+
+        if (amount > 0)
+        {
+            if(transform.position.x < -_maxMove) return;
+        }
+        else
+        {
+            if(transform.position.x > _maxMove ) return;
+        }
+
+        transform.position = new Vector3(transform.position.x - (amount * _speed * Time.deltaTime), transform.position.y, transform.position.z);
+    }
+}
