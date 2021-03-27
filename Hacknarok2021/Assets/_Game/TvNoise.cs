cee4dfe9fc7633db529b0f8e978b46ab357bf5d9
Oskar Kogut <kogutoskar123@gmail.com>
2021-03-27 14:12:47 +0100

    Add tv noise shader and prefab

---
+++ Hacknarok2021/Assets/_Game/TvNoise.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TvNoise : MonoBehaviour
+{
+    [SerializeField] private float _startingFactor = 0;
+    [SerializeField] private float _factorChangePerSecond = 1;
+
+    private float _currentFactor;
+    private Renderer _renderer;
+
+    private void Awake()
+    {
+        _currentFactor = _startingFactor;
+        _renderer = GetComponent<Renderer>();
+    }
+    
+    private void Update()
+    {
+        _currentFactor += _factorChangePerSecond * Time.deltaTime;
+        _renderer.sharedMaterial.SetFloat("_Factor1", _currentFactor);
+    }
+}
