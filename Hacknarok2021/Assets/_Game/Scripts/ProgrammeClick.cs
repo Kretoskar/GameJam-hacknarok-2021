cde27e1c0bbc97388e538fad36f49f651460d2fd
Oskar Kogut <kogutoskar123@gmail.com>
2021-03-27 15:44:06 +0100

    Add programme prefab

---
+++ Hacknarok2021/Assets/_Game/Scripts/ProgrammeClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgrammeClick : MonoBehaviour
+{
+    [SerializeField] private string _eventName = "";
+    
+    private ProgrammeSpawner _spawner;
+
+    private void Awake()
+    {
+        _spawner = FindObjectOfType<ProgrammeSpawner>();
+    }
+
+    public void Clicked()
+    {
+        _spawner.GetComponent<PlayMakerFSM>().SendEvent(_eventName);
+    }
+}
---
+++ Hacknarok2021/Assets/_Game/Scripts/ProgrammeSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgrammeSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _programme;
+    [SerializeField] private Transform _parent;
+
+    private GameObject _spawnedProgramme;
+    
+    public void Spawn()
+    {
+        _spawnedProgramme = Instantiate(_programme, _parent);
+    }
+
+    public void Destroy()
+    {
+        Destroy(_spawnedProgramme);
+        _spawnedProgramme = null;
+    }
+}
