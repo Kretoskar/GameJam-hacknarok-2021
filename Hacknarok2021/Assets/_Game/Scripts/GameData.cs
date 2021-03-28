47b7e46a58a04ce002f29109d2c0b8e084dba9b6
Oskar Kogut <kogutoskar123@gmail.com>
2021-03-28 06:39:09 +0200

    Add enum list for card types

--- Hacknarok2021/Assets/_Game/Scripts/GameData.cs
+++ Hacknarok2021/Assets/_Game/Scripts/GameData.cs
@@ -1,26 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "xczxc", fileName = "ads")]
 public class GameData : ScriptableObject
 {
     [SerializeField] private bool _wasSleeping = false;
-    [SerializeField] private List<Card> _allCardsPrefabs = null;
+    [SerializeField] private List<CardType> _cardsLeft = null;
     [SerializeField] private List<CardType> _availableCards = new List<CardType>();
 
+    private void OnEnable()
+    {
+        _cardsLeft = Enum.GetValues(typeof(CardType)).Cast<CardType>().ToList();
+    }
+    
     public bool WasSleeping
     {
         get => _wasSleeping;
         set { _wasSleeping = value; }
     }
 
-    public List<Card> AllCardsPrefabs => _allCardsPrefabs;
-
     public List<CardType> AvailableCards => _availableCards;
 
     public void AddAvailableCard(CardType cardType)
     {
+        _cardsLeft.Remove(cardType);
         _availableCards.Add(cardType);
     }
 
