62ba47836a3ab5ad4834878a39b93628bff6ad21
Piotr Ksiazek <pksiazek.py@gmail.com>
2021-03-28 02:59:15 +0100

    sprite downloader

---
+++ Hacknarok2021/Assets/GetTextureFromUrl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class GetTextureFromUrl : MonoBehaviour
+{
+    public Texture img;
+    private Sprite sprite;
+    private SpriteRenderer sr;
+    public string url = "https://tarotscraper.herokuapp.com/static/17.png";
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        StartCoroutine(DownloadImage(url));
+    }
+
+    IEnumerator DownloadImage(string MediaUrl)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+            Debug.Log(request.error);
+        else
+        {
+            img = ((DownloadHandlerTexture)request.downloadHandler).texture;
+            sprite = Sprite.Create((Texture2D)img, new Rect(0, 0, 1000, img.height), Vector2.zero);
+        }
+            
+    }
+}
---
+++ Hacknarok2021/Assets/_Game/Requests/Request.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Networking;
+
+public class Request : MonoBehaviour
+{
+    public string url;
+    TextMeshPro tm;
+    void Start()
+    {
+        url = "https://tarotscraper.herokuapp.com/";
+        tm = GetComponent<TextMeshPro>();
+        StartCoroutine(WebRequest());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator WebRequest()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+
+        yield return request.SendWebRequest();
+
+        if(request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log("Spierdoliło się");
+        }
+
+        else
+        {
+            tm.text = request.downloadHandler.text;
+        }
+    }
+}
